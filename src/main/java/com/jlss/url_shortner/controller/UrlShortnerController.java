edbf21d2d2353e430a0a6e767be6d95f12363f06
Jeet-solanki-1 <solankijeet285@gmail.com>
2026-04-05 05:57:26 +0530

    feat: add redirect view and utilized concurrent hash map for original url to a key mapping

---
+++ src/main/java/com/jlss/url_shortner/controller/UrlShortnerController.java
@@ -0,0 +1,26 @@
+package com.jlss.url_shortner.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.jlss.url_shortner.service.UrlShortnerService;
+import org.springframework.web.servlet.view.RedirectView;
+@RestController
+public class UrlShortnerController{
+	private final UrlShortnerService service;
+	public UrlShortnerController(UrlShortnerService service){
+		this.service=service;
+	}
+
+	@GetMapping("/api/original/")
+	public String shortenUrl(@RequestParam String url){
+		String resp = service.shortenUrl(url);
+		if (resp==null){
+			return "url for this code Not found or (get updated by another url as we only support 26 urls in our server)";
+		}
+		return resp;
+	}
+	@GetMapping("/api/{code}")
+	public RedirectView getOriginal(@PathVariable String code){
+		return new RedirectView(service.getOriginal(code));
+	}
+
+}
---
+++ src/main/java/com/jlss/url_shortner/service/UrlShortnerService.java
@@ -0,0 +1,35 @@
+package com.jlss.url_shortner.service;
+
+import org.springframework.stereotype.Service;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+
+@Service
+public class UrlShortnerService{
+
+	private final Map<String,String> urlStore = new ConcurrentHashMap<>();
+
+
+	/**
+	 * 
+	 * Accepts URL as String 
+	 * Actions create a hashmap entry (cunccurent)
+	 * Returns the short code as string
+	 * */
+	 String letters = "abcdefghijklmnopqrstuvwxyz";
+	int i=0;
+	public String shortenUrl(String url){
+		urlStore.putIfAbsent(String.valueOf(letters.charAt(i%letters.length())),url);
+		i++;
+		System.out.println("i: "+i+"next char/code: "+letters.charAt(i));
+		return urlStore.get(url);
+	}
+	public String getOriginal(String code){
+		if (urlStore.containsKey(code)){
+			return urlStore.get(code);
+		}
+		else{
+			return null;
+		}
+	}
+}
