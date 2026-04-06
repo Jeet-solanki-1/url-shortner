da0ab4fa8c5dd75f9a9c5b6a66f94a8c54795fe4
Jeet-solanki-1 <solankijeet285@gmail.com>
2026-04-07 05:17:06 +0530

    feat: add jakarata.validation.constraints .NotBlank and .Pattern annotation use in url attribute

--- src/main/java/com/jlss/url_shortner/model/RequestDTO.java
+++ src/main/java/com/jlss/url_shortner/model/RequestDTO.java
@@ -1,9 +1,15 @@
 package com.jlss.url_shortner.model;
 import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestDTO{
-	String url;
+	@NotBlank(message = "URL cannot be empty")
+	@Pattern(regexp = "^(http|https)://.*$",message="URL must start with http:// or https://")
+	private String url;
+	private Integer expiryMinutes;
 }
